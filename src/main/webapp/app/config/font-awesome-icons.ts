9d5ee0559067bf7ac44d5e580dacc00b4ad3a6d3
Kojo Ampia <kojo@jojoaddison.net>
2024-02-13 08:14:32 +0100

    dashboard with initial test

--- src/main/webapp/app/config/font-awesome-icons.ts
+++ src/main/webapp/app/config/font-awesome-icons.ts
@@ -37,6 +37,11 @@ import {
   faUsers,
   faUsersCog,
   faWrench,
+  faCamera,
+  faCameraAlt,
+  faCameraRetro,
+  faCameraRotate,
+  faVideoCamera,
   // jhipster-needle-add-icon-import
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -79,5 +84,10 @@ export const fontAwesomeIcons = [
   faUsers,
   faUsersCog,
   faWrench,
+  faCamera,
+  faCameraAlt,
+  faCameraRetro,
+  faCameraRotate,
+  faVideoCamera,
   // jhipster-needle-add-icon-import
 ];
