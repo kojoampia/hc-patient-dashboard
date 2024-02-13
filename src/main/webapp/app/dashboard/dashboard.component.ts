524acdc6cc0003d076b74fb2a07866390ce3ebdc
Kojo Ampia <kojo@jojoaddison.net>
2024-02-13 20:14:21 +0100

    Dashboard Service created

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Account } from 'app/core/auth/account.model';
 import SharedModule from 'app/shared/shared.module';
+import { Subject, takeUntil } from 'rxjs';
+import { DashboardService } from './dashboard.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-dashboard',
@@ -10,6 +13,34 @@ import SharedModule from 'app/shared/shared.module';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
   @Input() account!: Account;
+  phoneNumber!: string;
+  membership!: string;
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.fetchInformation(this.account.email);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  fetchInformation(email: string): void {
+    // phoneNumber = data.phoneNumber
+    // membership = data.membership
+    this.dashboardService
+      .fetchInformationByEmail(email)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (res: HttpResponse<any>) => {
+          this.phoneNumber = res.body.phoneNumber;
+          this.membership = res.body.membership;
+        },
+      });
+  }
 }
---
+++ src/main/webapp/app/dashboard/dashboard.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private resourceUrl = this.applicationConfigService.getEndpointFor('patientms/api/profiles');
+
+  constructor(
+    private http: HttpClient,
+    private applicationConfigService: ApplicationConfigService,
+  ) {}
+  fetchInformationByEmail(email: string): Observable<any> {
+    return this.http.get<any>(`${this.resourceUrl}/${email}`);
+  }
+}
