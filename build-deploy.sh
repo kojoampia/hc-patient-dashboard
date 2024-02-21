44f0bc178308c7c174730239d6cc93884421b830
Kojo Ampia <kojo@jojoaddison.net>
2024-02-22 00:53:16 +0100

    build deploy script

---
+++ build-deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+args=$#
+
+if [ $args -le 0 ];then
+    echo "version number required eg. 0.0.5"
+    exit 1
+fi
+
+echo "Synchronising with master on bitbucket"
+
+git pull -r
+
+# Set the version for the build and deploy
+export version=$1
+echo "Building and Deploying to Bedrock Admin Dashboard version $version"
+
+name=hc-patient-dashboard
+folder=`pwd`
+
+if [[ "$folder" != *"$name"* ]]; then
+  folder=$folder/$name
+fi
+
+echo "$folder"
+cd $folder
+
+
+echo "building..."
+docker build -t "hc-patient-dashboard" .
+echo "done."
+
+echo "tagging..."
+docker tag hc-patient-dashboard docker-registry.jojoaddison.net/hc-patient-dashboard:$version
+docker image ls | grep 'hc-patient-dashboard'
+echo "done."
+
+echo "pushing..."
+docker push docker-registry.jojoaddison.net/hc-patient-dashboard:$version
+echo "done."
+echo "build and deploy completed."
--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -4,7 +4,6 @@ import locale from '@angular/common/locales/en';
 import dayjs from 'dayjs/esm';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
@@ -16,7 +15,6 @@ import MainComponent from './layouts/main/main.component';
   standalone: true,
   template: '<jhi-main></jhi-main>',
   imports: [
-    BrowserAnimationsModule,
     MainComponent,
     // jhipster-needle-angular-add-module JHipster will add new module here
   ],
