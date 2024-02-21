43bc37077ae9790f671a392c263884ccabb6adc3
Kojo Ampia <kojo@jojoaddison.net>
2024-02-22 00:37:46 +0100

    dashboard and docker deployment updates

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -4,6 +4,7 @@ import locale from '@angular/common/locales/en';
 import dayjs from 'dayjs/esm';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
@@ -15,6 +16,7 @@ import MainComponent from './layouts/main/main.component';
   standalone: true,
   template: '<jhi-main></jhi-main>',
   imports: [
+    BrowserAnimationsModule,
     MainComponent,
     // jhipster-needle-angular-add-module JHipster will add new module here
   ],
